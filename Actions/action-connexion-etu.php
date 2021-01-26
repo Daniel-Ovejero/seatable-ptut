db79d86373cb46a8bd37a417b37271ef602fa5c4
kheloufi <djaber.kheloufi@edu-fcomte.fr>
2021-01-26 14:18:42 +0100

    Mot de passe oublié / premier mdp

--- Actions/action-connexion-etu.php
+++ Actions/action-connexion-etu.php
@@ -37,7 +37,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
 
     foreach ($rep as $etu => $value) {
         foreach ($value as $etudiant) {
-            if ($_POST["username"] == $etudiant["Name"]) {
+            if ($_POST["username"] == $etudiant["AdresseMail"]) {
                 if (password_verify($_POST["password"],$etudiant["MotDePasse"])) {
                     $_SESSION["row_id"] = $etudiant["_id"];
                     break;
