72c33ad9f0903360692b976a967544f3478c4bb5
Mathieu <Mathieu.dubayle@gmail.com>
2021-01-26 14:38:50 +0100

    change style connexion

--- Templates/connexion-etu.php
+++ Templates/connexion-etu.php
@@ -29,7 +29,7 @@ session_destroy();
         </div>
         <div class="mt-xl-5">
             <button class="btn btn-primary" type="submit">Connexion</button>
-            <a class="btn btn-secondary" style="width: 150px; height: 40px" href="mdp-oublie.php">Mot de passe oublié</a>
+            <a class="btn btn-secondary" style="width: 200px; height: 40px" href="mdp-oublie.php">Mot de passe oublié</a>
         </div>
     </form>
 </div>
