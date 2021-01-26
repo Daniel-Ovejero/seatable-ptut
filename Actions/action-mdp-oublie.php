d344df4b3af46873e81db881f0962974486cfefa
Mathieu <Mathieu.dubayle@gmail.com>
2021-01-26 12:05:40 +0100

    Ajout oublie de mot de passe

---
+++ Actions/action-mdp-oublie.php
@@ -0,0 +1,221 @@
+<?php
+session_start();
+
+require_once ('../Includes/conf.php');
+require_once("../Includes/PHPMailer-master/src/Exception.php");
+require_once("../Includes/PHPMailer-master/src/OAuth.php");
+require_once("../Includes/PHPMailer-master/src/PHPMailer.php");
+require_once("../Includes/PHPMailer-master/src/POP3.php");
+require_once("../Includes/PHPMailer-master/src/SMTP.php");
+use PHPMailer\PHPMailer\PHPMailer;
+
+// Envoi du mail
+
+if(isset($_POST["recup_mail"]) && $_POST["recup_mail"]) {
+    $mail = new PHPmailer();
+
+    $opts = array('http' =>
+        array(
+            'method' => 'GET',
+            'header' => "Content-Type: application/json\r\n" .
+                "Authorization: Token " . TOKEN . "\r\n",
+
+            'content' => '{
+        "filters":[
+
+		{
+
+			"column_name": "Name",
+
+			"filter_predicate": "is",
+
+			"filter_term": "' . $_POST["recup_mail"] . '",
+
+			"filter_term_modifier": ""	
+		}
+		],
+		"filter_conjunction": "And"
+	}'
+        )
+    );
+    $context = stream_context_create($opts);
+    $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/filtered-rows/?table_name=Eleve";
+    $result = file_get_contents($url, false, $context);
+    $result = json_decode($result);
+
+    if (sizeof($result->rows) < 1) {
+
+        $mail = new PHPmailer();
+
+        $mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP
+        $mail->Host = 'smtp.gmail.com'; // Spécifier le serveur SMTP
+        $mail->SMTPAuth = true; // Activer authentication SMTP
+        $mail->Username = 'djaberkheloufi@gmail.com'; // Votre adresse email d'envoi
+        $mail->Password = 'poulet25'; // Le mot de passe de cette adresse email
+        $mail->SMTPSecure = 'ssl'; // Accepter SSL
+        $mail->Port = 465;
+
+        $mail->setFrom('djaberkheloufi@gmail.com', 'Mailer'); // Personnaliser l'envoyeur
+        $mail->addAddress($_POST["recup_mail"]); // Ajouter le destinataire
+
+
+        $random_number = ''; // set up a blank string
+
+        $count = 0;
+
+        while ($count < 8) {
+            $random_digit = mt_rand(0, 9);
+
+            $random_number .= $random_digit;
+            $count++;
+        }
+
+        $opts = array('http' =>
+            array(
+                'method' => 'POST',
+                'header' => "Content-Type: application/json\r\n" .
+                    "Authorization: Token " . TOKEN . "\r\n",
+                'content' => '{
+   "row": {"Name": "' . $_POST['recup_mail'] . '",
+   "Code": "' . password_hash($random_number, PASSWORD_DEFAULT) . '"
+   },
+   "table_name": "Eleve"
+    }'
+            )
+        );
+
+        $context = stream_context_create($opts);
+        $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/rows/";
+        $result = file_get_contents($url, false, $context);
+
+        $mail->Subject = 'Verification email';
+        $mail->Body = $random_number;
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //$mail->SMTPDebug = 1;
+        $mail->send();
+        $_SESSION["Mail"] = $_POST["recup_mail"];
+
+        header('Location: ../Templates/mdp-oublie.php?section=code');
+    } else {
+        $error["doublon"] = true;
+    }
+
+} elseif (isset($_POST["verif_code"])) {
+
+    echo "lala";
+    $opts = array('http' =>
+        array(
+            'method' => 'GET',
+            'header' => "Content-Type: application/json\r\n" .
+                "Authorization: Token " . TOKEN . "\r\n",
+
+            'content' => '{
+        "filters":[
+
+		{
+
+			"column_name": "Name",
+
+			"filter_predicate": "is",
+
+			"filter_term": "' . $_SESSION["Mail"] . '",
+
+			"filter_term_modifier": ""	
+		}
+		],
+		"filter_conjunction": "And"
+	}'
+        )
+    );
+    $context = stream_context_create($opts);
+    $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/filtered-rows/?table_name=Eleve";
+    $result = file_get_contents($url, false, $context);
+    $rep = json_decode($result, true);
+
+
+    if (password_verify($_POST["verif_code"], $rep["rows"][0]["Code"])) {
+        $opts = array('http' =>
+            array(
+
+                'method' => 'PUT',
+                'header' => "Content-Type: application/json\r\n" .
+                    "Authorization: Token " . TOKEN . "\r\n",
+                'content' => '{
+   "row": {      "Code": "1" },
+   "table_name": "Eleve",
+   "row_id": "' . $rep["rows"][0]["_id"] . '"
+    }'
+            )
+
+        );
+        $context = stream_context_create($opts);
+        $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/rows/";
+        $result = file_get_contents($url, false, $context);
+        header("Location: ../Templates/mdp-oublie.php?section=changemdp");
+    } else {
+        $error["code"] = true;
+    }
+} elseif (isset($_POST["change_mdp"]) && isset($_POST["change_mdpc"])) {
+
+    $opts = array('http' =>
+        array(
+            'method' => 'GET',
+            'header' => "Content-Type: application/json\r\n" .
+                "Authorization: Token " . TOKEN . "\r\n",
+
+            'content' => '{
+        "filters":[
+
+		{
+
+			"column_name": "Name",
+
+			"filter_predicate": "is",
+
+			"filter_term": "' . $_SESSION["Mail"] . '",
+
+			"filter_term_modifier": ""	
+		}
+		],
+		"filter_conjunction": "And"
+	}'
+        )
+    );
+    $context = stream_context_create($opts);
+    $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/filtered-rows/?table_name=Eleve";
+    $result = file_get_contents($url, false, $context);
+    $rep = json_decode($result, true);
+
+    if ($_POST["change_mdp"] == $_POST["change_mdpc"]) {
+        //UPDATE
+        if ($rep["rows"][0]["Code"] == 1) {
+            $opts = array('http' =>
+                array(
+
+                    'method' => 'PUT',
+                    'header' => "Content-Type: application/json\r\n" .
+                        "Authorization: Token " . TOKEN . "\r\n",
+                    'content' => '{
+   "row": {   
+     
+   "MotDePasse": "' . password_hash($_POST['change_mdp'], PASSWORD_DEFAULT) . '",
+   "Active": "1",
+   "Code": "0"
+
+   },
+   "table_name": "Eleve",
+   "row_id": "' . $rep["rows"][0]["_id"] . '"
+    }'
+                )
+
+            );
+            $context = stream_context_create($opts);
+            $url = "https://cloud.seatable.io/dtable-server/api/v1/dtables/" . UUID . "/rows/";
+            $result = file_get_contents($url, false, $context);
+            header("Location: ../Templates/index.php");
+
+        }
+    } else {
+        $error['confirm_mdp'] = true;
+    }
+}
---
+++ Templates/mdp-oublie.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/index.css">
+</head>
+
+<body>
+    <div class="container text-center">
+        <h4 class="title-element">Récupération de mot de passe</h4>
+            <?php require_once('../Actions/action-mdp-oublie.php'); if(isset($_GET["section"]) && $_GET["section"] == 'code') { ?>
+                Un code de vérification vous a été envoyé par mail: <?php if(isset($_SESSION['Mail'])) echo $_SESSION['Mail'];?>
+                <br/>
+                <form class="connexionForm" method="post" action="">
+                    <div>
+                        <label for="verif_code">Code de Vérification</label>
+                    </div>
+                    <div>
+                        <input class="form-control main-input" id="verif_code" type="text" placeholder="Code de vérification" name="verif_code"/><br/>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary mt-2" type="submit">Valider</button>
+                    </div>
+                    <?php if(isset($error['code'])){?>
+                        <div class="alert alert-danger mt-4 w-50" style="margin-left: auto; margin-right: auto;" role="alert">
+                            Votre code n'est pas valide !
+                        </div>
+                    <?php }?>
+                </form>
+            <?php } elseif(isset($_GET["section"]) && $_GET["section"] == "changemdp") { ?>
+                Nouveau mot de passe pour <?php if(isset($_SESSION['Mail'])) echo $_SESSION['Mail']; ?>
+                <form class="connexionForm" method="post" action="">
+                    <input class="form-control main-input mt-2" type="password" placeholder="Nouveau mot de passe" name="change_mdp"/><br/>
+                    <input class="form-control main-input mt-2" type="password" placeholder="Confirmation du mot de passe" name="change_mdpc"/><br/>
+                    <button class="btn btn-primary mt-2" type="submit" value="Valider" name="change_submit">Valider</button>
+                    <?php if(isset($error['confirm_mdp'])){?>
+                        <div class="alert alert-danger mt-4 w-50" style="margin-left: auto; margin-right: auto;" role="alert">
+                            Mauvaise confirmation de votre mot de passe
+                        </div>
+                    <?php }?>
+                </form>
+            <?php } else { ?>
+                <form class="connexionForm" method="post" action="">
+                    <input class="form-control main-input" type="email" placeholder="Votre adresse mail" name="recup_mail"/><br/>
+                    <button class="btn btn-primary mt-2" type="submit" value="Valider" name="recup_submit">Valider</button>
+                </form>
+                <?php if(isset($error['doublon'])){?>
+                    <div class="alert alert-danger mt-4 w-50" style="margin-left: auto; margin-right: auto;" role="alert">
+                        Cette adresse email est déjà utilisée
+                    </div>
+                <?php }?>
+            <?php } ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
