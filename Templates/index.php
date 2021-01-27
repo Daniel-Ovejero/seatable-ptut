9f4e1cedf87ea6ed1aec88c17ff245b034e4d8cf
Firewolfs <fire95.wow@gmail.com>
2021-01-27 09:56:31 +0100

    index joli

--- Templates/index.php
+++ Templates/index.php
@@ -1,9 +1,20 @@
-<?php
-?>
-<html>
-
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <?php include '../Includes/import-css.php' ?>
+</head>
 <body>
-<a href="connexion-etu.php">Connexion Etudiante</a>
-<a href="connexion-prof.php">Connexion Professeur</a>
+    <div class="container h-100">
+        <div class="text-center align-self-center">
+            <img class="img-fluid" src="../Assets/images/logositeweb_72ppp.png" alt="Logo IUT">
+            <div class="mt-5">
+                <a class="btn btn-primary" href="connexion-etu.php"><i class="fas fa-user-graduate" style="font-size: 24px"></i><br> Connexion Etudiante</a>
+                <a class="btn btn-primary" href="connexion-prof.php"><i class="fas fa-user-tie" style="font-size: 24px"></i><br> Connexion Professeur</a>
+            </div>
+        </div>
+    </div>
+
+    <?php include '../Includes/import-js.php' ?>
 </body>
 </html>
