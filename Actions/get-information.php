4621ef9422d96c90e2e822e551eda7e5f1bd4529
Firewolfs <fire95.wow@gmail.com>
2021-01-29 09:58:00 +0100

    Début test page détail workflow

--- Actions/get-information.php
+++ Actions/get-information.php
@@ -2,6 +2,9 @@
 require_once '../Includes/conf.php';
 session_start();
 
+$banColumn = ['Id', 'Nom', 'Prenom', 'MotDePasse', 'Code'];
+$fields = [];
+
 if (!isset($_SESSION['row_id'])) { header('Location: index.php'); }
 
 $opts = [
@@ -23,9 +26,28 @@ $opts = [
     ]
 ];
 
+$opts1 = [
+    'http' => [
+        'method'  => 'GET',
+        'header'  => "Content-Type: application/json\r\n".
+            "Authorization: Token ".TOKEN."\r\n",
+    ]
+];
+
 $context  = stream_context_create($opts);
 $url =  "https://cloud.seatable.io/dtable-server/api/v1/dtables/".UUID."/filtered-rows/?table_name=Eleve";
 $result = file_get_contents($url, false, $context);
-$rep = json_decode($result);
+$rep = json_decode($result, true);
 
-$object = $rep->rows[0];
+$context1  = stream_context_create($opts1);
+$url1 =  "https://cloud.seatable.io/dtable-server/api/v1/dtables/".UUID."/columns/?table_name=".$_SESSION['statut'];
+$result1 = file_get_contents($url1, false, $context1);
+$result1 = json_decode($result1);
+
+foreach ($result1->columns as $column) {
+    if ($column->type !== "link" && !in_array($column->name, $banColumn)) {
+        $fields[] = $column;
+    }
+}
+
+$object = $rep["rows"][0];
