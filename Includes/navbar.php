0de14a6f1629ae128da5963101c0c7777905710a
Firewolfs <fire95.wow@gmail.com>
2021-02-24 12:06:42 +0100

    Ajout admission

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../Includes/conf.php';
 
-const BANCOLUMNS = ['Config'];
+const BANTABLES = ['Config'];
 
 if (session_status() === PHP_SESSION_DISABLED) { session_start(); }
 
@@ -13,10 +13,40 @@ $opts = [
     ]
 ];
 
+$optUser = [
+    'http' => [
+        'method' => 'GET',
+        'header'  => "Content-Type: application/json\r\n".
+            "Authorization: Token ".TOKEN."\r\n",
+        'content' =>  '{
+            "filters":[
+		        {
+                    "column_name": "Id",
+                    "filter_predicate": "is",
+                    "filter_term": "'.$_SESSION["row_id"].'",
+                    "filter_term_modifier": ""	
+		        }
+		    ],
+		    "filter_conjunction": "And"
+	    }'
+    ]
+];
+
 $context  = stream_context_create($opts);
+$contextUser  = stream_context_create($optUser);
+
 $url =  "https://cloud.seatable.io/dtable-server/api/v1/dtables/".UUID."/columns/?table_name=".$_SESSION['statut'];
+$urlConf = "https://cloud.seatable.io/dtable-server/api/v1/dtables/".UUID."/rows/?table_name=Config";
+$urlUser = "https://cloud.seatable.io/dtable-server/api/v1/dtables/".UUID."/filtered-rows/?table_name=".$_SESSION['statut'];
+
 $result = file_get_contents($url, false, $context);
 $result = json_decode($result);
+
+$configTables = file_get_contents($urlConf, false, $context);
+$configTables = json_decode($configTables);
+
+$user = file_get_contents($urlUser, false, $contextUser);
+$user = json_decode($user)->rows[0];
 ?>
         <nav class="navbar navbar-expand-lg navbar-light navbar-inverse" style="background-color: #e3f2fd;">
             <div class="container-fluid collapse navbar-collapse">
@@ -26,13 +56,21 @@ $result = json_decode($result);
                 <ul class="nav navbar-nav me-auto">
 <?php
 foreach ($result->columns as $res){
-    if($res->type == 'link' && !in_array($res->name, BANCOLUMNS)){
-//        if ()
+    if($res->type == 'link' && !in_array($res->name, BANTABLES)){
+        $visible = true;
+        foreach ($configTables->rows as $conf) {
+            if ($res->name == $conf->Table) {
+                $visible = $conf->Visible;
+                if (in_array($user->_id, $conf->Admin)) { $visible = true; }
+            }
+        }
+        if ($visible) {
 ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../Templates/<?= strtolower($res->name) ?>.php"><?= $res->name ?></a>
-                    </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../Templates/<?= strtolower($res->name) ?>.php"><?= $res->name ?></a>
+            </li>
 <?php
+        }
     }
 }
 ?>
