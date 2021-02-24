5cf20b1792e72b0f7167b36abf439bded77a574d
Teluep <teluep@gmail.com>
2021-02-24 14:29:08 +0100

    Page de la classe

--- Templates/classe.php
+++ Templates/classe.php
@@ -10,18 +10,21 @@
     <?php include_once '../Actions/get-classe.php' ?>
     <?php include_once '../Includes/navbar.php' ?>
 
-    <h1 class="text-center"><?php echo $idClasse ?></h1>
+    <div class="container">
+        <h2 class="mt-5"><?php echo $idClasse ?></h2>
 
-    <hr>
-    <div class="row justify-content-center">
-        <div class="col-3">
-            <ul class="list-group list-group-flush text-center">
-                <?php foreach ($eleves as $eleve){ ?>
-                    <li class="list-group-item"><?php echo $eleve->Nom." ".$eleve->Prenom ?></li>
-                <?php } ?>
-            </ul>
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-3">
+                <ul class="list-group text-center">
+                    <?php foreach ($eleves as $eleve){ ?>
+                        <li class="list-group-item"><?php echo $eleve->Nom." ".$eleve->Prenom ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
     </div>
+
 
 
 </body>
